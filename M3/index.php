d58eb122afeb3c6845279b76d0ea00826f399cff
Bryan Wood <woodywoodwoodman@gmail.comgit config --global user.email BDWood@users.noreply.github.com>
2015-04-22 16:38:39 -0700

    Added entire M3 project and some screenshots into the M2 folder

---
+++ M3/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Puppy Love</title>
+    <?php require_once 'links.php' ?>
+</head>
+<body>
+
+    <div class="top">
+        <div class="logo">
+            <span>Puppy Love</span>
+            <form action="">
+                <input type="text">
+                <button>Search</button>
+            </form>
+        </div>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Services</a>
+            <a href="#">Portfolio</a>
+            <a href="#">About</a>
+            <a href="#">Membership</a>
+            <a href="#">Testimonials</a>
+            <a href="#">FAQ</a>
+            <a href="#">Contact</a>
+        </nav>
+        <div class="background">
+            <header>
+                <h1>Great People, Better Dogs</h1>
+                <span>Welcome to the best site on the internet. We really hope
+                you buy our products because we really love to make money</span>
+            </header>
+        </div>
+    </div>
+
+    <div class="bottom">
+        <div>
+            
+        </div>
+        <div>
+            
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ M3/links.php
@@ -0,0 +1,6 @@
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' 
+type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' 
+type='text/css'>
+<link rel="stylesheet" href="normalize.css">
+<link rel="stylesheet" href="styles.css">
